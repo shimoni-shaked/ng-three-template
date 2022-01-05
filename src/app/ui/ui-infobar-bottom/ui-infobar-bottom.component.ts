ab78f4e4d961a16481fed01c14c6ae7b581e7542
shakeds5 <Shakeds5598@gmail.com>
2022-01-05 18:50:56 +0200

    style example

--- src/app/ui/ui-infobar-bottom/ui-infobar-bottom.component.ts
+++ src/app/ui/ui-infobar-bottom/ui-infobar-bottom.component.ts
@@ -1,15 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ui-infobar-bottom',
-  templateUrl: './ui-infobar-bottom.component.html'
+  templateUrl: './ui-infobar-bottom.component.html',
+  styleUrls: ['./ui-infobar-bottom.component.scss'],
 })
 export class UiInfobarBottomComponent implements OnInit {
+  public constructor() {}
 
-  public constructor() {
-  }
-
-  public ngOnInit(): void {
-  }
-
+  public ngOnInit(): void {}
 }
